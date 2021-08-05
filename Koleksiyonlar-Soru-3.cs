ce63746466febaed48b45705da86892d9db32660
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-05 21:36:14 +0300

    Add files via upload

---
+++ Koleksiyonlar-Soru-3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sesli = "aeıioöuü";
+            Console.WriteLine("Metin giriniz.");
+            string metin = Console.ReadLine();
+            ArrayList sesliHarfler = new ArrayList();
+
+            for (int i = 0; i < metin.Length; i++)
+            {
+                if (sesli.Contains(metin[i]))
+                {
+                    sesliHarfler.Add(metin[i]);
+                    //Console.Write(sesliHarfler);
+                }
+            }
+            sesliHarfler.Sort();
+            foreach (var item in sesliHarfler)
+            {
+                Console.Write(item);
+            }            
+        }
+    }
+}
